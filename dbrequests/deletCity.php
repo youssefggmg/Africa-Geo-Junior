6ef19b32de8cd53d6d579edeee9c1da7f4dc2f80
youssef elhoubi <elhoubiyoussef@gmail.com>
2024-12-13 17:26:33 +0100

    I've hade a buge for over 4 hours the code was corect but xammp had a nother opinion

--- dbrequests/deletCity.php
+++ dbrequests/deletCity.php
@@ -6,7 +6,7 @@ if (isset($_GET['cityID'])) {
 }
 $cityID = (int)mysqli_real_escape_string($connection,$cityID);
 
-$delete = mysqli_prepare($connection,"DELETE from city WHERE cityID = ?");
+$delete = mysqli_prepare($connection,"DELETE from city WHERE City_ID = ?");
 mysqli_stmt_bind_param($delete,"i",$cityID);
 mysqli_stmt_execute($delete);
 header("location: http://localhost/Africa-Geo-Junior/pages/citys.php?message=".urldecode("thecountryhave been deleted succssefuly"));
